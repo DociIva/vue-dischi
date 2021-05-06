accae741532493cf4b3f7aed245e7849b4a52314
DociIva <78482428+DociIva@users.noreply.github.com>
2021-05-06 16:35:09 +0200

    add-first-com

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+   <header class="containe"> 
+      
+   </header>
+</template>
+
+<script>
+export default {
+    name:'Header',
+}
+</script>
+
+<style scoped lang="scss">
+// header
+header {
+    width: 100%;
+    height: 100%;
+    background: #2e3a46;
+}
+
+</style>
